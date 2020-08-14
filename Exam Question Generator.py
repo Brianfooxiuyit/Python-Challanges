660863f4e8953971c8d4862a0721a0d8c27b8f1c
Brianfooxiuyit <69662494+Brianfooxiuyit@users.noreply.github.com>
2020-08-14 12:35:09 +0800

    Create Exam Question Generator.py

---
+++ Exam Question Generator.py
@@ -0,0 +1,4 @@
+import random
+Command_Words = ['analyse','define','outline', 'describe']
+Meme_Words = ['Abstraction', 'Sequence', 'Selection','Decomposition']
+print(Command_Words[random.randint(0,3)], Meme_Words[random.randint(0,3)])
